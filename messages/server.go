a53a5a056e56dd5ca85799e4c4258456fd27d366
Didier Roche <didrocks@ubuntu.com>
2017-02-03 16:35:13 +0100

    Add some identifier for websocket servers
    As we have 2 websocket servers, ensure logs are identified by URLs
    to help us when inspecting them.

--- messages/server.go
+++ messages/server.go
@@ -13,26 +13,29 @@ import (
 // Server maintaining the web socket server
 type Server struct {
 	// Messages in queue
-	Messages   <-chan *Action
-	clients    map[*Client]struct{}
-	register   chan *Client
-	unregister chan *Client
-	broadcast  chan *Action
-	err        chan error
-	quit       chan struct{}
+	Messages      chan *Action
+	clients       map[*Client]struct{}
+	register      chan *Client
+	unregister    chan *Client
+	broadcast     chan *Action
+	err           chan error
+	quit          chan struct{}
+	newClientFunc SendNewClient
+	res           string
 }
 
 // NewServer create a new ws server
 func NewServer(url string) *Server {
 
 	s := &Server{
-		Messages:   make(chan *Action),
-		clients:    make(map[*Client]struct{}),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-		broadcast:  make(chan *Action),
-		err:        make(chan error),
-		quit:       make(chan struct{}),
+		Messages:      make(chan *Action),
+		clients:       make(map[*Client]struct{}),
+		register:      make(chan *Client),
+		unregister:    make(chan *Client),
+		broadcast:     make(chan *Action),
+		err:           make(chan error),
+		quit:          make(chan struct{}),
+		res:           url,
 	}
 	http.Handle(url, websocket.Handler(s.onNewClient))
 
@@ -53,7 +56,7 @@ func (s *Server) onNewClient(conn *websocket.Conn) {
 
 	client, err := NewClient(conn, s)
 	if err != nil {
-		s.err <- fmt.Errorf("Couldn't accept new connection: %v", err)
+		s.err <- fmt.Errorf("Couldn't accept new connection on %s: %v", s.res, err)
 		conn.Close()
 		return
 	}
@@ -65,34 +68,34 @@ func (s *Server) onNewClient(conn *websocket.Conn) {
 
 // Listen to new ws client conn
 func (s *Server) Listen() {
-	log.Println("Start ws listener...")
+	log.Println("Start ws listener on", s.res)
 
 	for {
 		select {
 
 		// broadcast message to all clients
 		case msg := <-s.broadcast:
-			log.Println("Send to all clients:", msg)
+			log.Println("Send to all", s.res, "clients:", msg)
 			for c := range s.clients {
 				c.Send(msg)
 			}
 
 		// new client connected
 		case c := <-s.register:
-			log.Println("New client connected")
+			log.Println("New client connected on", s.res)
 			s.clients[c] = struct{}{}
-			log.Println("Now", len(s.clients), "clients connected.")
+			log.Println(len(s.clients), "clients connected on", s.res)
 			// TODO: send current page msg
 			//c.Send()
 
 		// client disconnected
 		case c := <-s.unregister:
-			log.Println("Disconnected client")
+			log.Println("Disconnected client from", s.res)
 			delete(s.clients, c)
 
 		// error reported
 		case err := <-s.err:
-			log.Printf("Error: %v\n", err)
+			log.Printf("Error: on %s %v\n", s.res, err)
 
 		// server shutdown
 		case <-s.quit:
