3de60ae563dd7ddd048ab852f8b1f04b720945a0
Didier Roche <didrocks@ubuntu.com>
2017-02-03 17:51:57 +0100

    Add VirtImages handler
    VirtImages maps to real image file on disk if any and default to nopreview.sgv.
    It's compatible with http.FileServer() for serving directly this file on disk.

---
+++ pilot/virtimages.go
@@ -0,0 +1,33 @@
+package pilot
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+)
+
+const defaultimg = "nopreview.svg"
+
+// VirtImages maps to real image file on disk if any. Default to nopreview.sgv
+// It's compatible with http.FileServer()
+type VirtImages struct{}
+
+// Open map to real image file on disk if any. Default to nopreview.sgv
+func (f VirtImages) Open(name string) (http.File, error) {
+	var file http.File
+	var err error
+	for _, p := range []string{
+		name,
+		path.Join(datadir, name),
+		path.Join(rootdir, name),
+		path.Join(rootdir, "www", defaultimg),
+	} {
+		fmt.Println(p)
+		if file, err = os.Open(p); err == nil {
+			return file, nil
+		}
+	}
+
+	return nil, fmt.Errorf("No image found")
+}
